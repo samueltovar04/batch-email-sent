1afd3555cca75592e04c536dbd30d05c744dd558
Samuel Tovar <tovar.samuel@gmail.com>
2025-09-22 10:24:35 -0400

    feat: creacion de estructura en clean arquitecture

--- src/main/java/org/blackfin/infrastructure/persistence/UserEntity.java
+++ src/main/java/org/blackfin/infrastructure/persistence/UserEntity.java
@@ -4,15 +4,13 @@ import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
 
 @Entity
-@Table(name = "userTable")
+@Table(name = "user_table")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity extends PanacheEntity {
